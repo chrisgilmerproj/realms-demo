3aa82acfe781baa6c8e5bd9d07d7c9998fcc941a
Chris Gilmer <chris.gilmer@gmail.com>
2017-04-21 14:45:02 -0700

    Add red button demo

--- realms_red_button.py
+++ realms_red_button.py
@@ -10,14 +10,11 @@ def main():
     ROBOT_IP = '192.168.1.110'
     with RealmsRobot(ROBOT_IP) as robot:
 
+        # PRESS RED BUTTON
         robot.center()
-
-        # GO TO COMPASS POINTS
-        for X in range(1, 3):
-            robot.up()
-            robot.down()
-            robot.left()
-            robot.right()
+        robot.close_gripper()
+        robot.press_red_button()
+        robot.center()
 
 
 if __name__ == "__main__":
